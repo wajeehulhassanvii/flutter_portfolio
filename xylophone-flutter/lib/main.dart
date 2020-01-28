ebe36df7abf276c27d0be3791ebb3a67f2b8e1dc
Wajeeh Ul Hassan <wajeehulhassan_vii@hotmail.com>
2020-01-29 01:54:42 +1000

    xylophone challenge app completed

---
+++ xylophone-flutter/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playAudioFile(int buttonNumber) {
+    final player = AudioCache();
+    player.play('note$buttonNumber.wav');
+  }
+
+  Expanded buildKey(int soundNumber, Color keyColor){
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playAudioFile(soundNumber);
+        },
+        color: keyColor,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.cyan),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
+          ), // column of buttons
+        ),
+      ),
+    );
+  }
+}
