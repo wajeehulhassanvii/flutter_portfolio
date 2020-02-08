f29c47628bb38aa671d3584850c5c2b3f6c6e58d
Wajeeh Ul Hassan <wajeehulhassan_vii@hotmail.com>
2020-02-08 16:10:14 +1000

    chat app

---
+++ Clima-Flutter/lib/services/weather.dart
@@ -0,0 +1,49 @@
+import 'package:clima/services/networking.dart';
+import 'package:clima/services/location.dart';
+
+const owm_api_key = 'ffb2030983d4ecf10edf3ab344eeb094';
+const owm_url='http://api.openweathermap.org/data/2.5/weather';
+
+class WeatherModel {
+
+  Future<dynamic> getLocationWeather() async{
+    Location location = Location();
+
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper('$owm_url?lat=${location.latitude}&lon=${location.longitude}&appid=$owm_api_key&units=metric');
+
+    var weatherData = await networkHelper.getData();
+  }
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
